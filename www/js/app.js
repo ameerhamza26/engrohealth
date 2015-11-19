5cd3d8979ef8622add2fa39bb825172d86a9cc0a
mtalhajamil <mtalhajamil93@gmail.com>
2015-11-19 11:48:03 +0500

    UI improved

--- www/js/app.js
+++ www/js/app.js
@@ -3,69 +3,58 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic' , 'starter.controllers', 'ngCordova', 'angularNumberPicker', 'ionic-toast'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'angularNumberPicker', 'ionic-toast'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-.config(function($stateProvider, $urlRouterProvider) {
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  }).
+  config(function ($stateProvider, $urlRouterProvider) {
 
     $stateProvider.state('app', {
-        url: "/app",
-      
-        templateUrl: "templates/menu.html",
-        controller: 'AppCtrl'
-    })
-	.state('map', {
+      url: "/app",
+
+      templateUrl: "templates/menu.html",
+      controller: 'AppCtrl'
+    }).
+      state('map', {
         url: "/map",
-      
+
         templateUrl: "templates/map.html",
         controller: 'mapCtrl'
-    })
-    .state('home', {
+      }).
+      state('home', {
         url: '/home',
         templateUrl: 'templates/home.html',
         controller: 'HomeCtrl'
-    })
-
-	.state('page2', {
-        url: '/page2',
-        templateUrl: 'templates/page2.html',
-        controller: ''
-    })
-	.state('profile', {
+      }).
+      state('profile', {
         url: '/profile',
         templateUrl: 'templates/profile.html',
         controller: 'profileCtrl'
-    })
-	.state('parking', {
-        url: '/parking',
-        templateUrl: 'templates/parking.html',
-        controller: 'parkingCtrl'
-    })
-	.state('civil', {
-        url: '/civil',
-        templateUrl: 'templates/civil.html',
-        controller: 'civilCtrl'
-    })
-	.state('auditorium', {
-        url: '/auditorium',
-        templateUrl: 'templates/auditorium.html',
-        controller: 'audiCtrl'
-    });
-	
+      }).
+      state('ping', {
+        url: '/ping',
+        templateUrl: 'templates/ping.html',
+        controller: 'PingCtrl'
+      }).
+      state('contact', {
+        url: '/contact',
+        templateUrl: 'templates/contact.html',
+        controller: 'ContactCtrl'
+      });
+
     $urlRouterProvider.otherwise('/home');
 
 
 
 
-});
+  });
--- www/js/controller.js
+++ www/js/controller.js
@@ -1,99 +1,193 @@
 
 
-angular.module('starter.controllers',[])
-.controller('AppCtrl', function($state) {
-	//$state.go('app.map');
+angular.module('starter.controllers', [])
+  .controller('AppCtrl', function ($state) {
+    //$state.go('app.map');
 
-	 	})
-.controller('mapCtrl', function( $ionicPlatform, $ionicModal,$ionicPopup,ionicToast,$scope, $cordovaGeolocation, $ionicLoading) {
-	//$state.go('profile');
+  })
+  .controller('mapCtrl', function ($ionicPlatform, $ionicModal, $ionicPopup, ionicToast, $scope, $cordovaGeolocation, $ionicLoading) {
+    //$state.go('profile');
 	
 	
-      document.addEventListener("deviceready", onDeviceReady, false);
-     
+    document.addEventListener("deviceready", onDeviceReady, false);
+
     function onDeviceReady() {
-        	  $ionicLoading.show({
-            template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
-        });
-         
-        var posOptions = {
-            enableHighAccuracy: true,
-            timeout: 20000,
-            maximumAge: 0
+      $ionicLoading.show({
+        template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+      });
+
+      var posOptions = {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0
+      };
+      $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+        var lat = 24.892249;
+        var long = 67.074715;
+
+        var myLatlng = new google.maps.LatLng(lat, long);
+
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
-            var lat  = 24.892249;
-            var long = 67.074715;
-             
-            var myLatlng = new google.maps.LatLng(lat, long);
-             
-            var mapOptions = {
-                center: myLatlng,
-                zoom: 16,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };          
-             
-            var map = new google.maps.Map(document.getElementById("map"), mapOptions);          
-             
-            $scope.map = map;   
-            $ionicLoading.hide();           
-             
-        }, function(err) {
-            $ionicLoading.hide();
-            console.log(err);
-        });
-    }              
-})
-.controller('HomeCtrl', function($ionicPlatform, $scope, $state,$cordovaLocalNotification, $http, $timeout, $interval, $ionicModal,$ionicPopup,ionicToast) {
-	
-	$scope.status = "true";
-	
-	 var myPopup = $ionicPopup.show({
-    template: '<input type="text" ></input>',
-    title: '<b>Enter Pass Key</b>',
-    subTitle: '',
-    scope: $scope,
-    buttons: [
-      { text: 'Cancel' },
-      {
-        text: '<b>Login</b>',
-        type: 'button button-assertive',
-        onTap: function(e) {
-    		$scope.status = "true";     
-		  	
+
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        $scope.map = map;
+        $ionicLoading.hide();
+
+      }, function (err) {
+        $ionicLoading.hide();
+        console.log(err);
+      });
+    }
+  })
+  .controller('HomeCtrl', function ($ionicPlatform, $cordovaInAppBrowser, $ionicLoading, $cordovaGeolocation, $scope, $state, $cordovaLocalNotification, $http, $timeout, $interval, $ionicModal, $ionicPopup, ionicToast) {
+
+    $scope.status = "true";
+
+    var myPopup = $ionicPopup.show({
+      template: '<input type="text" ></input>',
+      title: '<b>Enter Pass Key</b>',
+      subTitle: '',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: '<b>Login</b>',
+          type: 'button button-assertive',
+          onTap: function (e) {
+            $scope.status = "true";
+
+          }
         }
-      }
-    ]
-  });
-  myPopup.then(function(res) {
-    console.log('Tapped!', res);
-		$scope.status = "true";
-  });
-		
-        $scope.notify = function() {
-			$state.go('map');
+      ]
+    });
+    myPopup.then(function (res) {
+      console.log('Tapped!', res);
+      $scope.status = "true";
+    });
 
-        };
-		 $scope.profile = function() {
-			$state.go('profile');
 
-        };
-		 $scope.parking = function() {
-			$state.go('parking');
 
-        };
-		$scope.audi = function() {
-			$state.go('auditorium');
+    $scope.notify = function () {
+      $state.go('map');
 
-        };
-			$scope.civil = function() {
-			$state.go('civil');
+    };
+    $scope.profile = function () {
+      $state.go('profile');
 
-        };
-})
-.controller('profileCtrl', function($ionicPlatform, $ionicModal,$ionicPopup,$scope, $state,$cordovaLocalNotification, $http, $timeout, $interval,ionicToast) {
+    };
+
+    $scope.contact = function () {
+      $state.go('contact');
+    };
+
+    $scope.ping = function () {
+      $state.go('ping');
+
+    };
+
+    $scope.form = function () {
+
+      var options = {
+        location: 'no',
+        clearcache: 'yes',
+        toolbar: 'no',
+        hardwareback: 'no',
+        clearsessioncache: 'yes'
+      };
+
+      $cordovaInAppBrowser.open('https://www.dropbox.com/s/l0leid0f4uzahoj/In-Patient%20Claim%20Form.pdf', '_system', options)
+        .then(function (event) {
+          // success
+        })
+        .catch(function (event) {
+          alert("Error");
+        });
+
+      //$cordovaInAppBrowser.close();
+      //window.open('https://www.dropbox.com/s/l0leid0f4uzahoj/In-Patient%20Claim%20Form.pdf', '_system', 'location=yes')
+
+    };
+
+    $scope.location = function () {
+      $ionicLoading.show({
+        template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+      });
+      var posOptions = { timeout: 10000, enableHighAccuracy: false };
+      $cordovaGeolocation
+        .getCurrentPosition(posOptions)
+        .then(function (position) {
+          var lat = position.coords.latitude;
+          var long = position.coords.longitude;
+          $ionicLoading.hide();
+          $ionicPopup.alert({
+            title: "longitute:" + long + "  latitute:" + lat
+          }).then(function (res) {
+
+          });
+        }, function (err) {
+          $ionicLoading.hide();
+          alert("Cannot get location due to: " + err)
+        });
+
+    };
+
+  })
+  .controller('profileCtrl', function ($ionicPlatform, $cordovaSms, $ionicModal, $ionicPopup, $scope, $state, $cordovaLocalNotification, $http, $timeout, $interval, ionicToast) {
+
+
+
+  }).controller('PingCtrl', function ($cordovaSms, $scope, $ionicPopup) {
 	
-	
+    // $scope.SMSText = "";
+    // $cordovaSms
+    //     .send('03126995968', 'SMS content', 'options')
+    //     .then(function() {
+    //       alert('SMS was sent');
+    //     }, function(error) {
+    //       alert('SMS sending failed');
+    //     });
   
-});
+    $scope.listdata = [];
+     
+    //$scope.listdata.push({ "SMS" : "empty" });
+    $scope.sendSMS = function () {
+      $cordovaSms
+        .send('03028285155', $scope.listdata[0], 'options')
+        .then(function () {
+          $ionicPopup.alert({
+            title: 'SMS Sent!!'
+          }).then(function (res) {
+
+          });
+        }, function (error) {
+          alert('SMS sending failed');
+        });
+    }
+
+  }).controller('ContactCtrl', function ($cordovaSms, $scope, $state, $ionicPopup) {
+
+    $scope.listdata = [];
+    $scope.listdata.push({ "name": "Medical Hotline South (Dr. Salman) 1", "number": "03002018246" });
+    $scope.listdata.push({ "name": "Medical Hotline South (Dr. Salman) 2", "number": "0340004489" });
+    $scope.listdata.push({ "name": "Landline Number", "number": "02132410054" });
+    $scope.listdata.push({ "name": "Call Centre", "number": "0800-00242" });
+    $scope.listdata.push({ "name": "Medical Hotline Central (Dr. Munazza)", "number": "03317333200" });
+    $scope.listdata.push({ "name": "Manager Relationship (Mohsin Murtaza Hussain)", "number": "03028228254" });
+    $scope.listdata.push({ "name": "Officer Relationship (Zafar Ahmed)", "number": "03400004468" });
+    $scope.listdata.push({ "name": "Head of Misc. Dept. (Shaikh Babar)", "number": "0302-8297044" });
+
+    $scope.showContact = function (num) {
+      $ionicPopup.alert({
+        title: '<b>' + $scope.listdata[num].name + '<br />' + $scope.listdata[num].number + '</b>'
+      }).then(function (res) {
+
+      });
+    }
+
+  });
 
